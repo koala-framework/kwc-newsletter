59bea35d6b87eba60f967fc2a780a6455d3eb1e8
Alexander Kaufmann <ak@vivid-planet.com>
2018-02-26 07:33:48 +0100

    added jobs for deleting old subscribers (non activated and unsubscribed)
    - delete unsubscribed subscribers after one year
    - delete non activated subscribers after one week

---
+++ Bundle/MaintenanceJob/DeleteUnsubscribedJob.php
@@ -0,0 +1,51 @@
+<?php
+namespace KwcNewsletter\Bundle\MaintenanceJob;
+
+use KwfBundle\MaintenanceJobs\AbstractJob;
+use Psr\Log\LoggerInterface;
+
+class DeleteUnsubscribedJob extends AbstractJob
+{
+    private $subscribersModel;
+
+    public function __construct(\Kwf_Model_Abstract $model)
+    {
+        $this->subscribersModel = $model;
+    }
+
+    public function getFrequency()
+    {
+        return self::FREQUENCY_DAILY;
+    }
+
+    public function execute(LoggerInterface $logger)
+    {
+        $select = new \Kwf_Model_Select();
+        $select->whereEquals('unsubscribed', true);
+        $select->where(new \Kwf_Model_Select_Expr_LowerEqual(
+            new \Kwf_Model_Select_Expr_Field('last_unsubscribe_date'),
+            new \Kwf_Date(strtotime('-1 year'))
+        ));
+
+        $ids = array_map(
+            function($subscriber) { return $subscriber['id']; },
+            $this->subscribersModel->export(\Kwf_Model_Abstract::FORMAT_ARRAY, $select, array('columns' => array('id')))
+        );
+        $count = count($ids);
+        if ($count > 0) {
+            $select = new \Kwf_Model_Select();
+            $select->whereEquals('id', $ids);
+            $this->subscribersModel->deleteRows($select);
+
+            $select = new \Kwf_Model_Select();
+            $select->whereEquals('subscriber_id', $ids);
+            $this->subscribersModel->getDependentModel('Logs')->deleteRows($select);
+
+            if ($this->subscribersModel instanceof \KwcNewsletter_Kwc_NewsletterCategory_Subscribe_Model) {
+                $this->subscribersModel->getDependentModel('ToCategory')->deleteRows($select);
+            }
+        }
+
+        $logger->debug("Deleted $count subscribers");
+    }
+}
--- KwcNewsletter/Kwc/Newsletter/Subscribe/Model.php
+++ KwcNewsletter/Kwc/Newsletter/Subscribe/Model.php
@@ -22,5 +22,20 @@ class KwcNewsletter_Kwc_Newsletter_Subscribe_Model extends Kwf_Model_Db
     {
         $this->_dependentModels['Logs'] = 'KwcNewsletter_Kwc_Newsletter_Subscribe_LogsModel';
         parent::_init();
+
+        $abstractSelect = new \Kwf_Model_Select();
+        $abstractSelect->order('date', 'DESC');
+
+        $select = clone $abstractSelect;
+        $select->whereEquals('state', 'subscribed');
+        $this->_exprs['last_subscribe_date'] = new Kwf_Model_Select_Expr_Child_First('Logs', 'date', $select);
+
+        $select = clone $abstractSelect;
+        $select->whereEquals('state', 'activated');
+        $this->_exprs['last_activated_date'] = new Kwf_Model_Select_Expr_Child_First('Logs', 'date', $select);
+
+        $select = clone $abstractSelect;
+        $select->whereEquals('state', 'unsubscribed');
+        $this->_exprs['last_unsubscribe_date'] = new Kwf_Model_Select_Expr_Child_First('Logs', 'date', $select);
     }
 }
