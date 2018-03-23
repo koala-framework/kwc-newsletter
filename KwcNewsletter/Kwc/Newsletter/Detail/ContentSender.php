de417f266b8bb491913acb6db06918b9fe02adcd
Ricky Smith <rs@vivid-planet.com>
2018-03-23 12:37:11 +0100

    Remove unnecessary template in Newsletter_Detail

--- KwcNewsletter/Kwc/Newsletter/Detail/ContentSender.php
+++ KwcNewsletter/Kwc/Newsletter/Detail/ContentSender.php
@@ -3,10 +3,6 @@ class KwcNewsletter_Kwc_Newsletter_Detail_ContentSender extends Kwf_Component_Ab
 {
     public function sendContent($includeMaster)
     {
-        //show content only in preview, else a 404
-        if (!Kwf_Component_Data_Root::getShowInvisible()) {
-            throw new Kwf_Exception_NotFound();
-        }
-        parent::sendContent($includeMaster);
+        throw new Kwf_Exception_NotFound();
     }
 }
