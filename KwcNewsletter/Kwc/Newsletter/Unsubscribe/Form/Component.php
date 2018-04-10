8fc6d4dd453067bbf7aa09606c82f6b4cb0da399
Alexander Kaufmann <ak@vivid-planet.com>
2018-04-10 15:10:07 +0200

    fix parse recipieint in unsubscribe form component
    use helper function from mail redirect

--- KwcNewsletter/Kwc/Newsletter/Unsubscribe/Form/Component.php
+++ KwcNewsletter/Kwc/Newsletter/Unsubscribe/Form/Component.php
@@ -1,5 +1,5 @@
 <?php
-class KwcNewsletter_Kwc_Newsletter_Unsubscribe_Form_Component extends Kwc_Form_Component
+class KwcNewsletter_Kwc_Newsletter_Unsubscribe_Form_Component extends Kwc_Form_NonAjax_Component
 {
     public $_recipient; //set by KwcNewsletter_Kwc_Newsletter_Unsubscribe_Component
 
@@ -44,35 +44,12 @@ class KwcNewsletter_Kwc_Newsletter_Unsubscribe_Form_Component extends Kwc_Form_C
 
     public function processInput(array $postData)
     {
-        if (!$this->_recipient && isset($postData['d'])) {
-            /**
-             * 0 = redirectId
-             * 1 = recipientId
-             * 2 = recipientModelShortcut
-             * 3 = hash
-             */
-            $params = explode('_', $postData['d']);
-            if (count($params) >= 4 && $params[3] == $this->_getHash(array(
-                    $params[0], $params[1], $params[2]
-                ))) {
-                $c = Kwf_Component_Data_Root::getInstance()->getComponentByClass('KwcNewsletter_Kwc_Newsletter_Detail_Mail_Component', array(
-                    'limit' => 1,
-                    'subroot' => $this->getData()
-                ));
-                if ($c) {
-                    $model = null;
-                    foreach ($c->getComponent()->getRecipientSources() as $key=>$value) {
-                        if (is_array($value) && $key == $params[2]) {
-                            $model = $value['model'];
-                        }
-                    }
-                    if ($model) {
-                        $this->_recipient = Kwf_Model_Abstract::getInstance($model)
-                            ->getRow($params[1]);
-                    }
-                }
-            }
+        if (!$this->_recipient && isset($postData['recipient'])) {
+            $recipient = Kwc_Mail_Redirect_Component::parseRecipientParam($postData['recipient']);
+            if ($recipient) $this->_recipient = $recipient;
         }
+
+        parent::processInput($postData);
     }
 
     private function _getHash(array $hashData)
