31eec9af887369cd3a356ad1a74a1787d0458f14
Alexander Kaufmann <ak@vivid-planet.com>
2018-05-07 08:56:41 +0200

    set configuration of bundle as paramaters and added this to services
    is better for reading the code

--- Bundle/DependencyInjection/KwcNewsletterExtension.php
+++ Bundle/DependencyInjection/KwcNewsletterExtension.php
@@ -16,11 +16,8 @@ class KwcNewsletterExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $def = $container->getDefinition('kwc_newsletter.maintenance_job.delete_unsubscribed');
-        $def->addArgument($config['subscribers']['delete_unsubscribed_after_days']);
-
-        $def = $container->getDefinition('kwc_newsletter.maintenance_job.delete_not_activated');
-        $def->addArgument($config['subscribers']['delete_not_activated_after_days']);
+        $container->setParameter('kwc_newsletter.subscribers.delete_unsubscribed_after_days', $config['subscribers']['delete_unsubscribed_after_days']);
+        $container->setParameter('kwc_newsletter.subscribers.delete_not_activated_after_days', $config['subscribers']['delete_not_activated_after_days']);
     }
 
 }
