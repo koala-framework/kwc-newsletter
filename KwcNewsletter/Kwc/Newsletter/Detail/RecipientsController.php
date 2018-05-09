cffeb8b3aaaa5a396e73a35dc7dbe6307db44176
Alexander Kaufmann <ak@vivid-planet.com>
2018-05-09 11:07:56 +0200

    recipients in mail tab of newsletter can now be filtered with recipients from other sended newsletter
    is useful if you want to send a newsletter with the same queue of the other

--- KwcNewsletter/Kwc/Newsletter/Detail/RecipientsController.php
+++ KwcNewsletter/Kwc/Newsletter/Detail/RecipientsController.php
@@ -13,7 +13,48 @@ class KwcNewsletter_Kwc_Newsletter_Detail_RecipientsController extends KwcNewsle
             'skipWhere' => true
         );
 
+        $this->_filters['other_newsletter_id'] = array(
+            'type' => 'ComboBox',
+            'label' => trlKwf('Other Newsletter').':',
+            'width' => 150,
+            'listWidth' => 250,
+            'skipWhere' => true,
+            'data' => $this->_getOtherNewsletters(),
+            'defaultText' => trlKwf('all')
+        );
+
         unset($this->_columns['edit']);
+    }
+
+    protected function _getOtherNewsletters()
+    {
+        $ret = array();
+
+        $mailComponentIds = array();
+        $select = new Kwf_Model_Select();
+        $select->whereEquals('status', 'finished');
+        $select->where(new Kwf_Model_Select_Expr_Higher(
+            new Kwf_Model_Select_Expr_Field('create_date'),
+            new Kwf_Date(strtotime("-1 year"))
+        ));
+        foreach (Kwf_Model_Abstract::getInstance('KwcNewsletter\Bundle\Model\Newsletters')->export(
+            Kwf_Model_Abstract::FORMAT_ARRAY, $select, array('columns' => array('id', 'component_id'))
+        ) as $row) {
+            $mailComponentIds[] = "{$row['component_id']}_{$row['id']}_mail";
+        }
+
+        $select = new Kwf_Model_Select();
+        $select->whereEquals('component_id', $mailComponentIds);
+        $select->order(new Zend_Db_Expr('LENGTH(component_id)'), 'DESC');
+        $select->order('component_id', 'DESC');
+        foreach (Kwf_Model_Abstract::getInstance('Kwc_Mail_Model')->export(
+            Kwf_Model_Abstract::FORMAT_ARRAY, $select, array('columns' => array('component_id', 'subject'))
+        ) as $row) {
+            preg_match('#\_([0-9]+)\_mail$#i', $row['component_id'], $match);
+            $ret[] = array($match[1], $row['subject']);
+        }
+
+        return $ret;
     }
 
     protected function _isAllowedComponent()
@@ -34,6 +75,19 @@ class KwcNewsletter_Kwc_Newsletter_Detail_RecipientsController extends KwcNewsle
 
         if ($lastActivatedDate = $this->_getParam('query_last_activated_date')) {
             $ret->where(new Kwf_Model_Select_Expr_LowerEqual('last_activated_date', new Kwf_Date($lastActivatedDate)));
+        }
+
+        if (($otherNewsletterId = $this->_getParam('query_other_newsletter_id')) && $otherNewsletterId !== 'all') {
+            $select = new Kwf_Model_Select();
+            $select->whereEquals('newsletter_id', $otherNewsletterId);
+            $select->whereEquals('recipient_model', get_class($this->_getModel()));
+
+            $ret->whereEquals('id', array_map(
+                function ($row) { return $row['recipient_id']; },
+                Kwf_Model_Abstract::getInstance('KwcNewsletter\Bundle\Model\NewsletterQueueLogs')->export(
+                    Kwf_Model_Abstract::FORMAT_ARRAY, $select, array('columns' => array('recipient_id'))
+                )
+            ));
         }
 
         return $ret;
