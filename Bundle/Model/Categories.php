3712daf743a9a6e062ced0094d62c9611438ced8
Alexander Kaufmann <ak@vivid-planet.com>
2018-05-14 11:08:04 +0200

    show statistics for every category how many subscribers where activated, not or unsubscribed

--- Bundle/Model/Categories.php
+++ Bundle/Model/Categories.php
@@ -11,6 +11,20 @@ class Categories extends \Kwf_Model_Db
         $this->_dependentModels['ToSubscribers'] = 'KwcNewsletter\Bundle\Model\SubscribersToCategories';
 
         parent::_init();
+
+        $select = new \Kwf_Model_Select();
+        $select->whereEquals('subscriber_activated', false);
+        $select->whereEquals('subscriber_unsubscribed', false);
+        $this->_exprs['count_not_activated'] = new \Kwf_Model_Select_Expr_Child_Count('ToSubscribers', $select);
+
+        $select = new \Kwf_Model_Select();
+        $select->whereEquals('subscriber_activated', true);
+        $select->whereEquals('subscriber_unsubscribed', false);
+        $this->_exprs['count_activated'] = new \Kwf_Model_Select_Expr_Child_Count('ToSubscribers', $select);
+
+        $select = new \Kwf_Model_Select();
+        $select->whereEquals('subscriber_unsubscribed', true);
+        $this->_exprs['count_unsubscribed'] = new \Kwf_Model_Select_Expr_Child_Count('ToSubscribers', $select);
     }
 
     protected function _setupFilters()
--- KwcNewsletter/Kwc/Newsletter/CategoriesController.php
+++ KwcNewsletter/Kwc/Newsletter/CategoriesController.php
@@ -3,12 +3,17 @@ class KwcNewsletter_Kwc_Newsletter_CategoriesController extends Kwf_Controller_A
 {
     protected $_modelName = 'KwcNewsletter\Bundle\Model\Categories';
     protected $_position = 'pos';
+    protected $_buttons = array('csv');
 
     protected function _initColumns()
     {
         $this->_columns->add(new Kwf_Grid_Column('category', trlKwf('Category'), 200))
             ->setEditor(new Kwf_Form_Field_TextField());
         $this->_columns->add(new Kwf_Grid_Column('id', trlKwf('id'), 40));
+
+        $this->_columns->add(new Kwf_Grid_Column('count_not_activated', trlKwf('not activated')));
+        $this->_columns->add(new Kwf_Grid_Column('count_activated', trlKwf('active')));
+        $this->_columns->add(new Kwf_Grid_Column('count_unsubscribed', trlKwf('unsubscribed')));
     }
 
     protected function _getSelect()
